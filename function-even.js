f8c159a9cd4e7c8a6596596000a6c71da3f560ac
bopathirs <boopathirs111@gmail.com>
2022-06-30 09:20:30 +0530

    tassk14

---
+++ function-even.js
@@ -0,0 +1,9 @@
+let a;
+function even(num){
+    if(num%2==0){
+        console.log(`${num} is even number`);
+    }else{
+    console.log(`${num} is not even number`);
+    }
+}
+let result = even(39);
